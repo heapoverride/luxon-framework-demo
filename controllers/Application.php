512a31718b8df072b88fc7eb044abecdf08c91ad
Unreal Security <arran.bishop89@aol.com>
2021-06-13 19:27:59 +0300

    Updated

--- controllers/Application.php
+++ controllers/Application.php
@@ -41,6 +41,78 @@ class Application {
     }
 
     /**
+     * Render the admin page for news
+     */
+    static function NewsAdmin() {
+        $doc = new Document();
+        $doc->title = "News Admin";
+
+        $doc->main->add(
+            ($formContainer = new Html\Div())->add(
+                ($form = new Html\Form())->add(
+                    (new Html\Table())
+                        ->addRow(
+                            (new Html\Label())
+                                ->add("Article title:")
+                                ->set("for", "ar_title"),
+                            (new Html\Input)
+                                ->setName("title")
+                                ->set("type", "text")
+                                ->set("autocomplete", "off")
+                                ->setId("ar_title")
+                                ->setName("title")
+                        )
+                        ->addRow(
+                            (new Html\Label())
+                                ->add("Article body:")
+                                ->set("for", "ar_body"),
+                            (new Html\Textarea)
+                                ->setName("body")
+                                ->set("autocomplete", "off")
+                                ->setId("ar_body")
+                        )
+                        ->addRow(
+                            "",
+                            (new Html\Input)
+                                ->set("type", "submit")
+                                ->set("value", "Add article")
+                        )
+                )
+            ),
+            ($articlesContainer = new Html\Div())->add(
+                Article::getArticles()
+            )
+        );
+
+        $formContainer->set("class", "form-container");
+        $form->set("method", "post");
+
+        $doc->html();
+    }
+
+    /**
+     * Handle new article submission
+     */
+    static function NewsAdminPost() {
+        $title = $_POST['title'];
+        $body = $_POST['body'];
+
+        $result = ORM::instance()
+            ->insert("articles", [
+                "title" => $title,
+                "body"  => $body,
+                "time"  => time()
+            ])
+            ->exec();
+
+        if ($result->isError) {
+            // ...
+        }
+
+        header("Location: /news/admin/");
+    }
+
+    /**
      * Render the test page
      */
     static function Test() {
