f080f6369b32f84e168ef95c6f06e8e9f3e878bb
Unreal Security <arran.bishop89@aol.com>
2021-06-18 20:08:04 +0300

    A dirty hack

--- other/components/Markdown.php
+++ other/components/Markdown.php
@@ -5,33 +5,24 @@
  * with custom component
  */
 
-class Markdown extends Html\Element {
+class Markdown extends Html\Text {
     public $md;
 
     function __construct($markdown = null)
     {
-        parent::__construct("");
-        if ($markdown !== null) { $this->md = $markdown; }
-    }
-
-    function html($return = false, $format = false) {
-        $parser = new Parsedown();
-        $html = $parser->text($this->md);
-
-        if ($return) {
-            return $html;
-        } else {
-            echo $html;
-        }
+        parent::__construct();
+        parent::setEscapeText(false);
+        
+        if ($markdown) parent::setText((new Parsedown())->parse($markdown));
     }
 
     static function fromFile($filepath) {
-        $data = "";
-        
+        $markdown = "";
+
         if (file_exists($filepath)) {
-            $data = file_get_contents($filepath);
+            $markdown = file_get_contents($filepath);
         }
 
-        return new Markdown($data);
+        return new Markdown($markdown);
     }
 }
